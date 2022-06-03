80d401e39c7b6882c5ac47c05377757db22e8a96
mkamka <miikka.kajanne@wunderdog.fi>
2022-06-03 23:19:26 +0300

    poistettu omaa koodia delecated actionista

--- src/main/java/fi/vm/sade/cas/oppija/configuration/DelegatedAuthenticationActionConfiguration.java
+++ src/main/java/fi/vm/sade/cas/oppija/configuration/DelegatedAuthenticationActionConfiguration.java
@@ -1,45 +1,59 @@
 package fi.vm.sade.cas.oppija.configuration;
 
+import fi.vm.sade.cas.oppija.configuration.action.Pac4jClientProvider;
+import lombok.val;
 import org.apereo.cas.web.flow.CasWebflowConstants;
 import org.apereo.cas.web.flow.DelegatedClientAuthenticationAction;
 import org.apereo.cas.web.flow.DelegatedClientAuthenticationConfigurationContext;
 import org.apereo.cas.web.flow.DelegatedClientAuthenticationWebflowManager;
+import org.apereo.cas.web.support.WebUtils;
+import org.pac4j.core.context.JEEContext;
+import org.pac4j.core.context.session.SessionStore;
+import org.pac4j.core.exception.http.HttpAction;
+import org.pac4j.saml.client.SAML2Client;
 import org.pac4j.saml.exceptions.SAMLException;
+import org.pac4j.saml.profile.SAML2Profile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.webflow.execution.Action;
 import org.springframework.webflow.execution.Event;
 import org.springframework.webflow.execution.RequestContext;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static fi.vm.sade.cas.oppija.CasOppijaConstants.PARAMETER_SAML_RELAY_STATE;
+import static fi.vm.sade.cas.oppija.CasOppijaConstants.REQUEST_SCOPE_ATTRIBUTE_SAML_LOGOUT;
+
 @Configuration
 public class DelegatedAuthenticationActionConfiguration {
     // override default delegatedAuthenticationAction to automatically logout on error
     static final String TRANSITION_ID_LOGOUT = "logout";
+    private Pac4jClientProvider clientProvider;
+    private SessionStore sessionStore;
     @Bean
     public Action delegatedAuthenticationAction(
             final DelegatedClientAuthenticationConfigurationContext context,
             final DelegatedClientAuthenticationWebflowManager delegatedClientAuthenticationWebflowManager
     ) {
         return new DelegatedClientAuthenticationAction(context, delegatedClientAuthenticationWebflowManager ) {
-            /*@Override
-            public Event doExecute(RequestContext context) {
-                HttpServletRequest request = WebUtils.getHttpServletRequestFromExternalWebflowContext(context);
-                val response = WebUtils.getHttpServletResponseFromExternalWebflowContext(context);
+            @Override
+            public Event doExecute(RequestContext requestContext) {
+                HttpServletRequest request = WebUtils.getHttpServletRequestFromExternalWebflowContext(requestContext);
+                HttpServletResponse response = WebUtils.getHttpServletResponseFromExternalWebflowContext(requestContext);
+                JEEContext context = new JEEContext(request, response);
                 if (isLogoutRequest(request)) {
                     try {
-                        val webContext = new JEEContext(request, response);
-                        val clientName = retrieveClientName(webContext);
-                        Service service = populateContextWithService(context, webContext, clientName);
-                        val client = findDelegatedClientByName(request, clientName, service);
-                        WebUtils.putDelegatedAuthenticationClientName(context, client.getName());
-                        populateContextWithClientCredential(client, webContext, context);
-                        return null;
-                    } catch (HttpAction e) {
-                        return handleLogout(e, context, response);
-                    }
+                        SAML2Profile profile = requestContext.getRequestScope().get(REQUEST_SCOPE_ATTRIBUTE_SAML_LOGOUT, SAML2Profile.class);
+                        SAML2Client client = (SAML2Client) clientProvider.getClient(profile);
+                        val samlContext = client.getContextProvider().buildContext(client, context, sessionStore);
+                        client.getLogoutProfileHandler().receive(samlContext);
+                        } catch (final HttpAction action) {
+                            return handleLogout(action, requestContext);
+                        }
                 }
-                return super.doExecute(context);
-            }*/
+                return super.doExecute(requestContext);
+            }
 
             @Override
             protected Event stopWebflow(Exception e, RequestContext requestContext) {
@@ -49,14 +63,14 @@ public class DelegatedAuthenticationActionConfiguration {
                 return super.stopWebflow(e, requestContext);
             }
 
-           /* private Event handleLogout(HttpAction httpAction, RequestContext requestContext, HttpServletResponse response) {
+           private Event handleLogout(HttpAction httpAction, RequestContext requestContext) {
                 if (httpAction.getCode() == 200) {
                     String redirectUrl = requestContext.getRequestParameters().get(PARAMETER_SAML_RELAY_STATE);
                     WebUtils.putLogoutRedirectUrl(requestContext, redirectUrl);
                     return result(TRANSITION_ID_LOGOUT);
                 }
                 throw new IllegalArgumentException("Unhandled logout response code: " + httpAction.getCode());
-            }*/
+            }
         };
     }
 }
